13ab249e0c318305327aba0abb00502dd24a74c4
User <user@MacBook-Air-de-User.local>
2025-04-18 15:05:30 -0400

    feat: se puebran con exito las rutas de registro de usuario, logueo de usuario y obtencion de todos los usuarios.

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -7,16 +7,18 @@ import { authMiddleware } from '../middlewares/auth.middleware.js';
 const router = Router()
 
 
-// // Endpoint para registrar un nuevo usuario
-router.post('/register', register);
+// Endpoint para registrar un nuevo usuario
+router.post('/register', register);  //Ruta probada con exito
 
-// // Endpoint para obtener todos los usuarios
-router.get('/users', authMiddleware, verifyAdmin, getAllUsers);
+// Endpoint para obtener todos los usuarios (solo admin)
+router.get('/users', authMiddleware, verifyAdmin, getAllUsers);  //Ruta probada con exito
 
-// // Endpoint para loguear un usuario
-router.post('/login', login);
+// Endpoint para loguear un usuario
+router.post('/login', login);  //Ruta probada con exito
 
-// // Endpoint para actualizar un usuario por id
-router.put('/update/:id', authMiddleware, verifyAdmin, updateUserById);
+// Endpoint para actualizar un usuario por id
+router.put('/update/:id', updateUserById);
 
 export default router;  
+
+// authMiddleware, verifyAdmin, 
